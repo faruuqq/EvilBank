f6c15f784e89981e44c2c858a7b84a41a17eaa65
Faruuq <faruuq.qayyum@accenture.com>
2024-01-15 16:39:15 +0700

    feat: add initial files

---
+++ EvilBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EvilBank
+//
+//  Created by Faruuq Qayyum on 15/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
